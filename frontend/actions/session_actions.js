ef83d2881cc73527fef6a2f847c50d32e0f80a70
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-14 15:42:38 -0700

    adds session actions to session_actions.js

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+import * as SessionAPIUtil from './util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => (
+  SessionAPIUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const login = user => dispatch => (
+  SessionAPIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout()
+    .then(user => dispatch(receiveCurrentUser(null)))
+);
