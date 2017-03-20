86217f75445fdc9f6d15ee9254b254ddaa5eced0
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-20 08:56:23 -0700

    pre-merge LikesAndComments

---
+++ app/models/comment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  user_id    :integer          not null
+#  photo_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ApplicationRecord
+  validates :user, :photo, :body, presence: true
+
+  belongs_to :user
+  belongs_to :photo
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316173931) do
+ActiveRecord::Schema.define(version: 20170320072506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text     "url",        null: false
