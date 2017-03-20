b11d7c576d444eb2c017f74da976bc8db21af936
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-20 01:35:13 -0700

    Updated Models. Need to nest under API

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  photo_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Like < ApplicationRecord
+  validates :user, :photo, presence: true
+  validates :photo, uniqueness: { scope: :user }
+
+  belongs_to :user
+  belongs_to :photo
 end
--- db/migrate/20170320082614_create_comments.rb
+++ db/migrate/20170320082614_create_comments.rb
@@ -7,7 +7,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
-    add_index :comments, :user_id
-    add_index :comments, :photo_id
+    add_index :comments, :user_id, name: "index_comments_on_photo_id"
+    add_index :comments, :photo_id, name: "index_comments_on_user_id"
   end
 end
