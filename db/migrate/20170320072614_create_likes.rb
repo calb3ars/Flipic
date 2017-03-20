ce4fbd3dde6d25c03cdc51c7c6f609db47754b87
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-20 01:28:49 -0700

    Likes migration complete. Comment model created

--- db/migrate/20170320072614_create_likes.rb
+++ db/migrate/20170320072614_create_likes.rb
@@ -5,6 +5,7 @@ class CreateLikes < ActiveRecord::Migration[5.0]
       t.integer :photo_id, null: false
 
       t.timestamps
+       t.index ["user_id", "photo_id"], name: "unique_likes_on_user_id_and_photo_id", unique: true
     end
     add_index :likes, :photo_id
     add_index :likes, :user_id
