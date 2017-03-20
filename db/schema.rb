39d730ea914007d954b5cb654858f6b3bbf7c17a
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-03-20 09:11:31 -0700

    Updated DB and migrations cleaned

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170320082614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "photo_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       null: false
